1e17e41976993b250ac09f48a1a7ea60c3ca9f88
Tientrn <trannhattien160802@gmail.com>
2023-05-16 23:14:26 +0700

    Functional Components

--- demoareact/src/Players.js
+++ demoareact/src/Players.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
 import { Players } from './shared/ListOfPlayers'
-export default class Player extends Component {
-    render() {
-        return (
-            <div className='container' >
+export default function Player() {
 
-                {
-                    Players.map((Player) => (
-                        <div className='column'>
-                            <div className='card'>
-                                <img src={Player.img} />
-                                <h3>{Player.name} </h3>
-                                <p className='title'>{Player.club}</p>
-                                <p><button>Detail</button></p>
-                            </div>
+    return (
+        <div className='container' >
+
+            {
+                Players.map((Player) => (
+                    <div className='column'>
+                        <div className='card'>
+                            <img src={Player.img} />
+                            <h3>{Player.name} </h3>
+                            <p className='title'>{Player.club}</p>
+                            <p><button>Detail</button></p>
                         </div>
-                    ))
-                }
-            </div>
-        )
-    }
+                    </div>
+                ))
+            }
+        </div>
+    )
+
 
 }
